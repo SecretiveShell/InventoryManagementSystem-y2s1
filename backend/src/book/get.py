06d2eee22d4b47fa214bb15ee9d754a3800cc6c0
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-11-08 12:04:41 +0000

    make get book endpoint functional

--- backend/src/book/get.py
+++ backend/src/book/get.py
@@ -1,6 +1,17 @@
 from .router import router
-from models import Book
+from models import Book as BookModel
+from database.session import Session
+from database.ORM import Book
+from sqlalchemy import select
+from fastapi import HTTPException
 
 @router.get("/get")
-async def get_book(id: int) -> Book:
-    pass
+async def get_book(id: int) -> BookModel:
+    with Session() as session:
+        command = select(Book).where(Book.book_id == id)
+        book_instance = session.execute(command).scalar_one_or_none() 
+
+    if book_instance is None:
+        raise HTTPException(404, detail="book ID does not exist")
+ 
+    return BookModel.model_validate(book_instance, from_attributes=True)
