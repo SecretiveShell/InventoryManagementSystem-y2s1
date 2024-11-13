ef5a6f555b2226dabb6157f89b8106fd1d791a93
Hussla <94571595+Hussla@users.noreply.github.com>
2024-11-13 11:14:05 +0000

    Add author router

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from .book import router as book_router
 from .inventory import router as inventory_router
+from .author import router as author_router
 
 router = APIRouter(
     prefix="/api",
@@ -8,3 +9,4 @@ router = APIRouter(
 
 router.include_router(book_router)
 router.include_router(inventory_router)
+router.include_router(author_router)
