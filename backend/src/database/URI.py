b0e8f09880a7cc0a91892248003c636ad593c472
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-11-15 10:47:50 +0000

    remove dbapi print statement

--- backend/src/database/URI.py
+++ backend/src/database/URI.py
@@ -6,4 +6,3 @@ host = os.getenv("B4B_DB_HOST", "db:5432")
 schema = os.getenv("B4B_DB_SCHEMA", "db")
 
 DATABASE_URI = f"postgresql+psycopg2://{user}:{password}@{host}/{schema}"
-print(DATABASE_URI)
