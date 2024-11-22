96f9be1e4c1f3496d3878b78e05ce8adfa783f33
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-11-22 10:39:09 +0000

    add login code

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -1,7 +1,11 @@
+import secrets
 from typing import Annotated, Literal
 from fastapi import APIRouter, Depends
-from auth.login import delete_session
+from auth.login import create_session, delete_session
+from auth.redis_user_model import RedisUserModel
+from database.ORM import User
 from models.auth import LoginRequest, LoginResponse
+from database.session import Session
 
 router = APIRouter(
     prefix="/auth",
@@ -13,7 +17,18 @@ router = APIRouter(
 async def login(login: LoginRequest) -> LoginResponse:
     # TODO: Implement login functionality
 
-    return LoginResponse(success=True, token="token")
+    with Session() as session:
+        user = session.query(User).filter(User.email == login.email).first()
+
+    if user is None:
+        return LoginResponse(success=False)
+
+    if secrets.compare_digest(str(user.password), login.password):
+        model = RedisUserModel.model_validate(user, from_attributes=True)
+        token = await create_session(model)
+        return LoginResponse(success=True, token=token)
+
+    return LoginResponse(success=False)
 
 
 @router.post("/logout")
