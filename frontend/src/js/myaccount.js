23f7e8b6ee532e1e751d379786b05f6b7eee7a4b
kacperbl <c0025162@hallam.shu.ac.uk>
2024-12-04 11:03:43 +0000

    fixed navigation on myaccount.html

--- frontend/src/js/myaccount.js
+++ frontend/src/js/myaccount.js
@@ -69,3 +69,26 @@ function validatePassword(password) {
 
     return { isValid: true };
 }
+
+// Navigation button handlers
+document.querySelectorAll('nav button').forEach(button => {
+    button.addEventListener('click', (e) => {
+        const buttonText = e.target.textContent.toLowerCase().trim();
+        
+        switch(buttonText) {
+            case 'browse':
+                window.location.href = 'inventory.html';
+                break;
+            case 'cart':
+                window.location.href = 'cartview.html';
+                break;
+            case 'my account':
+                window.location.href = 'myaccount.html';
+                break;
+            case 'log out':
+                handleLogout();
+                break;
+        }
+    });
+});
+
